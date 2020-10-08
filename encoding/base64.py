380f6f3a2fc2cd2dcecaf0a1c0541118781c8959
Dysta <xav4098@gmail.com>
2020-10-08 12:52:35 +0200

    refonte docstring for base64

--- encoding/base64.py
+++ encoding/base64.py
@@ -3,84 +3,70 @@ class Base64:
                       'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
                       'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
                       'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_']
-    _a: int = 97
-    _A: int = 65
-    _0: int = 48
 
     def ord(char: str) -> int:
         """
         Convert the given single char value in base64
-        :parameter
-        char : str
-            The char to convert
-        :return
-            The int value, between 0 and 63 included
-        :raises
-            Exception when character is outside the charset
-            Exception when parameter is not a single char
+
+        :param char: The char to convert
+        :return: The int value, between 0 and 63 included
+        :raise Exception: when character is outside the charset or when parameter is not a single char
+        :rtype: int
         """
         if len(char) > 1:
             raise Exception('Incorrect parameter: expected a single character')
-        if char[0] not in Base64._CHARSET and not char == '-' and not char == '_':
+        if char[0] not in Base64._CHARSET:
             raise Exception('Incorrect parameter: invalid char value')
 
         if 'a' <= char <= 'z':
-            return int(char) - Base64._a
+            return ord(char) - ord('a')
 
         if 'A' <= char <= 'Z':
-            return int(char) - Base64._A + 26
+            return ord(char) - ord('A') + 26
 
         if '0' <= char <= '9':
-            return int(char) - Base64._0 + 52
+            return ord(char) - ord('0') + 52
 
         if char == '-':
             return 62
-        elif char == '_':
-            return 63
-        else:
-            raise Exception('Incorrect parameter: invalid char value')
+
+        return 63 # char is equal to '_'
 
 
     def chr(value: int) -> str:
         """
         Get the base64 character from the given value
-        :parameter
-        value : int
-            The value to convert
-        :return:
-            Encoded value
-        :raise
-            Exception when value is not in the charset
+
+        :param value: The value to convert
+        :return: Encoded value
+        :raise Exception: when value is not in the charset
+        :rtype: str
         """
         if not value < len(Base64._CHARSET):
             raise Exception('Incorrect parameter: invalid value')
 
         return Base64._CHARSET[value]
 
-    def chrMod(value: int):
+    def chrMod(value: int) -> str:
         """
         Get the base64 character from the given value after applying a modulo on it
         to ensure the call will not fail
-        :parameter
-        value : int
-            The value to convert
-        :return:
-            Encoded value
+
+        :parameter value: The value to convert
+        :return: Encoded value
+        :rtype: str
         """
         return Base64._CHARSET[value % len(Base64._CHARSET)]
 
     def encode(value: int, length: int) -> str:
         """
         Encode a int value to pseudo base64
-        :parameter
-        value: int
-            value to encode
-        length: int
-            then expected result length. Must be in range [1-6]
-        :return:
-            the decoded value
-        :raise
-            Exception when the encoded string is not in range [1-6]
+
+        :param value: value to encode
+        :param length: then expected result length. Must be in range [1-6]
+        :return: the decoded value
+        :raise Exception: when the encoded string is not in range [1-6]
+        :rtype: str
         """
         if length < 1 or length > 6:
             raise Exception('Incorrect parameter: l must be in range [1-6]')
@@ -97,13 +83,11 @@ class Base64:
     def decode(encoded: str) -> int:
         """
         Decode pseudo base64 value to int
-        :parameter
-        encoded : str
-            the encoded value
-        :return:
-            the decoded value
-        :raise
-            Exception when the encoded string is not in range [1-6]
+
+        :param encoded: the encoded value
+        :return: the decoded value
+        :raise Exception: when the encoded string is not in range [1-6]
+        :rtype: int
         """
         if len(encoded) > 6:
             raise Exception('Incorret parameter: parameter too long')

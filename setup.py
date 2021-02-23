6c6899b84aac42af16372d05dbd4ad19c3995c34
Dysta <xav4098@gmail.com>
2021-02-23 18:12:33 +0100

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='Dofutils',
-    version='1.0.1',
+    version='0.0.1',
     packages=find_packages(exclude="tests"),
     url='https://github.com/Dysta/Dofutils',
     license='',

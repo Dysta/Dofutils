d8df0b9ef4f19294f0de13f5248db890a01f66de
Dysta <xav4098@gmail.com>
2021-02-23 13:29:31 +0100

    modification setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='Dofutils',
     version='1.0.0',
@@ -8,5 +11,7 @@ setup(
     license='',
     author='Dysta',
     author_email='',
-    description='Collection of useful things for build Dofus Retro bot/emulator '
+    description='Collection of useful things for build Dofus Retro bot/emulator ',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )

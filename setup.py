3b12c69c256e6ff1ff9a697231468d04f1b21d19
Dysta <xav4098@gmail.com>
2021-02-23 13:21:09 +0100

    ajout setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Dofutils',
+    version='1.0.0',
+    packages=find_packages(exclude="tests"),
+    url='https://github.com/Dysta/Dofutils',
+    license='',
+    author='Dysta',
+    author_email='',
+    description='Collection of useful things for build Dofus Retro bot/emulator '
+)

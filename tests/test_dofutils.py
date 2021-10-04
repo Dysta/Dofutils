2ec627fc3264edfda7ce22092efafac58bfc094b
Dysta <xav4098@gmail.com>
2021-10-04 22:16:52 +0200

    Update version

--- tests/test_dofutils.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-def launch() -> None:
-    """
-    Run all unittests. Equivalent to:
-    `poetry run python -u -m unittest discover`
-    """
-    subprocess.run(["python", "-m", "unittest", "discover", "-v"])
